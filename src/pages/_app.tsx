bf5d29ef360094fc23376ab89ebf99c5cf5bb094
Hachikoi-the-creator <facka72@gmail.com>
2023-05-31 00:29:32 -0500

    fixed ts v2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,6 @@ import "~/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  // @ts-expect-error
+  // @ts-ignore
   return <Component {...pageProps} />;
 }
